663b32afff8b3865c41de48f5114e756eed7dda2
perevedniuk <perevedniuk@gmail.com>
2022-12-22 21:17:52 +0200

    init project

--- MovieDB/Application/SceneDelegate.swift
+++ MovieDB/Application/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: scene.coordinateSpace.bounds)
         window?.windowScene = scene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
         window?.makeKeyAndVisible()
     }
 
